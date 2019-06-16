d25e442c1258e3edc3dd22ff41444b3e4bf6bffa
Praslar <pxthang97@gmail.com>
2019-06-16 17:32:32 +0700

    Update profiles

--- Back_end/PHP/Labs/inclues/dbh.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-	 $conn=mysqli_connect("localhost","root","","phpLabs");
-        if(!$conn){
-            die("Connection failed: ".mysqli_connect_error());
-        } else {
-            echo "Connection Successfully!";
-        }
